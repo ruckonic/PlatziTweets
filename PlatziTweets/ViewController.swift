bba1e1c07ed39f5cfde265feb35664727762de22
Rene Corrales <ruckonic@icloud.com>
2020-06-05 12:05:36 -0600

    Initial Commit

---
+++ PlatziTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziTweets
+//
+//  Created by Rene Corrales on 6/5/20.
+//  Copyright © 2020 Rene Corrales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
